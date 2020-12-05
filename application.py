7f82bf5328e4a54873f253a0f6e4002ae9fd61df
BrunoCoric <bruno.coric@fer.hr>
2020-12-05 13:10:03 +0100

    Opet

--- application.py
+++ application.py
@@ -15,22 +15,22 @@ def allowed_file(filename):
 def render_page():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        file = request.files.get('image')
-        if file is None or file.filename == "":
-            return jsonify({'error': 'no file'})
-        if not allowed_file(file.filename):
-            return jsonify({'error': 'format not supported'})
-
-        try:
-            img_bytes = file.read()
-            image = io.BytesIO(img_bytes)
-            image = Image.open(image)
-            image = getExpression(image)
-            tensor = transform_image(image)
-            prediction,ocitano = get_prediction(tensor)
-            return render_template('rezultat.html',ocitano = ocitano)
-        except:
-            return jsonify({'error': 'error during prediction'})
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     if request.method == 'POST':
+#         file = request.files.get('image')
+#         if file is None or file.filename == "":
+#             return jsonify({'error': 'no file'})
+#         if not allowed_file(file.filename):
+#             return jsonify({'error': 'format not supported'})
+#
+#         try:
+#             img_bytes = file.read()
+#             image = io.BytesIO(img_bytes)
+#             image = Image.open(image)
+#             image = getExpression(image)
+#             tensor = transform_image(image)
+#             prediction,ocitano = get_prediction(tensor)
+#             return render_template('rezultat.html', rez=prediction,ocitano = ocitano)
+#         except:
+#             return jsonify({'error': 'error during prediction'})
